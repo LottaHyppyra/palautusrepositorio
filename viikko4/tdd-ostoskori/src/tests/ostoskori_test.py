21e7e0ab3b09fd250b5dd4932e845fa79a24319c
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-22 18:50:19 +0200

    ostoskorin hinta on tuotteiden summa testi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -29,3 +29,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(voi)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+
+        self.assertEqual(self.kori.hinta(), 5)     
